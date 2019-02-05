f5ca97e014a8bd5d53ac6bfe806f9ca81da5b4a1
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-05 17:51:09 -0600

    Demonstrate basics of switch statements.

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-// IF-ELSE challenge question
-function checkNumber(number) {
-	if (number < 5) {
-		return "Tiny";
-	} else if (number < 10) {
-		return "Small";
-	} else if (number < 15) {
-		return "Medium";
-	} else if (number < 20) {
-		return "Large";
-	} else {
-		return "Huge";
+//switch uses STRICT EQUALITY CHECKING, ===
+function caseInSwitch(val) {
+	var answer = "";
+	switch (val) {
+			case 1:
+				answer = "alpha";
+				break;
+			case 2:
+				answer = "beta";
+				break;
+			case 3:
+				answer = "gamma";
+				break;
+			case 4:·
+				answer = "delta";
+				break;
+			default:
+				answer = "Invalid selection";
+				break;
 	}
+
+	return answer;
 }
 
-console.log(checkNumber(20));
+console.log(caseInSwitch(5));
 
-//write a function to return golf nickname given a stroke count
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
-
-function golfScore(par, strokes) {
-	if (strokes == 1) {
-		return names[0];
-	} else if (strokes <= par - 2) {
-		return names[1];
-	} else if (strokes == par - 1) {
-		return names[2];
-	} else if (strokes == par) {
-		return names[3];
-	} else if (strokes == par + 1) {
-		return names[4];
-	} else if (strokes == par + 2) {
-		return names[5];
-	} else if (strokes >= par + 3) {
-		return names[6];
+function sequentialSizes(val) {
+	var answer = "";
+	switch (val) {
+		case 1:
+		case 2:
+		case 3:
+			answer = "Low";
+			break;
+		case 4:
+		case 5:
+		case 6:
+			answer = "Mid";
+			break;
+		case 7:
+		case 8:
+		case 9:
+			answer = "High";
+			break;
 	}
+	return answer;
 }
 
-console.log(golfScore(5, 4));
+console.log(sequentialSizes(8));
+
+function isLess(a, b) {
+	return a < b
+}
+
+console.log(isLess(20, 5));
+
+//return early from a function
+function abTest(a, b) {
+	if (a < 0 || b < 0) {
+		return undefined;
+	}
+}
+console.log(abTest(-2, 2));
