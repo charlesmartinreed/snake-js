349e61c70c3514796094abc7e185220c3c05d60b
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-05 16:55:42 -0600

    Demonstrate difference between equality and strict equality operators.

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ var testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+//IF STATEMENTS
+function ourTrueOrFalse(isItTrue) {
+	if (isItTrue) {
+		return "Yes, it's true";
+	}
+	return "No, it's false"
+}
+
+var result = ourTrueOrFalse(5>2); //because Javascript is so damn loose, I can simply evaluate this statement within the arg block
+console.log(result)
+
+// == vs ===
+// == uses coercion to attempt to match values, "12" IS EQUAL to 12.
+// ===, the strict equality operator, does NOT use type conversion
+function testEqual(val) {
+	if (val == 12) {
+		return "Equal"
+	}
+	return "Not Equal"
+}
+
+console.log(testEqual("12"));
+
+function testStrictEqual(val) {
+	if (val === 12) {
+		return "Equal"
+	}
+	return "Not Equal"
+}
+
+console.log(testStrictEqual("12"));
