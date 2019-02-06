cd142be81f9a8a907f99ae23982e8cd271d47f43
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 17:56:22 -0600

    Demonstrate getters and setters.

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-//objects can contain functions
-const bicycle = {
-	gear: 2,
-	setGear(newGear) {
-		"use strict";
-		this.gear = newGear;
+//GETTERS AND SETTERS
+class Book {
+	constructor(author) {
+		this._author = author; //private variable
+	}
+
+	//GETTER - return the private variable to the user without giving access to alter it
+	get writer() {
+		return this._author;
+	}
+
+	//SETTER - allows setting of private variable without exposing its current contents. Can invoke calculations or just outright overwrite it
+	set writer(updatedAuthor) {
+		this._author = updatedAuthor;
 	}
 }
 
-bicycle.setGear(3);
-console.log(bicycle.gear);
-
-//ES6 can create objects using class keyword
-
-//old way of creating objects, using new
-var spaceShuttle = function(targetPlanet) {
-	this.targetPlanet = targetPlanet;
-}
-var zeus = new spaceShuttle('Jupiter'); //create new Object
-
-console.log(zeus.targetPlanet)
-
-//new way of creating objects, using class and constructor
-class ES6SpaceShuttle {
-	constructor(targetPlanet) {
-		this.targetPlanet = targetPlanet;
-	}
-}
-var artemis = new ES6SpaceShuttle('Mercury');
-console.log(artemis.targetPlanet);
-
+//create thermostat class, make a getter and setter. Accept F, return temp in celsius
 function makeClass() {
-	class Vegetable {
-		constructor(name) {
-			this.name = name;
+	class Thermostat {
+		constructor(temperature) { //F
+			//convert to C
+			this._temperature = 5/9 * (temperature - 32); //this means only accessible in class, _ means private variable
+		}
+
+		get temperature() {
+			//let celsius = (this._temperature - 32) * (5/9);
+			return this._temperature;
+		}
+
+		set temperature(updatedTemp) {
+			this._temperature = updatedTemp;
 		}
 	}
 
-	return Vegetable;
+	return Thermostat;
 }
 
-const Vegetable = makeClass();
-const carrot = new Vegetable('carrot');
-console.log(carrot.name); //carrot
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); //this will be converted to C - 24.44444 C
+let temp = thermos.temperature; //uses getter
+//thermos.temperature = 26; //setter
+//temp = thermos.temperature;
+console.log(temp);
