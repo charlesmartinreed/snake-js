8682cebcc4d163dc02377d4eaf3900c92892a44f
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 16:14:42 -0600

    Demonstrate Object.freeze for making const obj/array read only.

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-//const is "read only"
-// function printManyTimes(str) {
-// 	"use strict";
-//
-// 	const SENTENCE = str + " is cool!"; //common practice is all caps for const
-//
-// 	SENTENCE = str + " is amazing!" //invalid! "Assignment to constant variable"
-//
-// 	for(let i = 0; i < str.length; i+=2) {
-// 		console.log(SENTENCE);
-// 	}
-// }
-
-//printManyTimes("freeCodeCamp");
-
-//however, you can still mutate individual elements of a const array
-const s = [5, 7, 2];
-function editInPlace() {
+function freezeObj() {
 	"use strict";
 
-	//s = [2, 5, 7]; //invalid
+	//create this constant - can currently be changed
+	const MATH_CONSTANTS = {
+		PI: 3.14
+	};
 
-	//valid
-	s[0] = 2;
-	s[1] = 5;
-	s[2] = 7;
+	Object.freeze(MATH_CONSTANTS); //prevents mutation of const objects and arrays - makes MATH_CONSTANTS a "read only"
 
-	return s
+	try {
+		MATH_CONSTANTS.PI = 99;
+	} catch(ex) {
+		console.log(ex); //if this doesn't work, return this exception
+	}
+	//otherwise, return the altered PI
+	return MATH_CONSTANTS.PI;
 }
 
-console.log(editInPlace());
+const PI = freezeObj();
+
+console.log(PI);
