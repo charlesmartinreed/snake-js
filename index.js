c9d75bbfa123bd40192470060cfafbb2dc09e69d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 16:02:00 -0600

    Demonstrate var vs. let.

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ function checkSign(num) {
 console.log(checkSign(-10));
 console.log(checkSign(20));
 console.log(checkSign(0));
+
+// VAR vs LET
+let catName = "Quincy";
+let quote;
+
+//let catName = "Beau"; //valid with var, not valid with let
+
+function catTalk() {
+	"use strict"; //catches common coding mistakes and invalid actions
+
+	catName = "Oliver";
+	quote = catName = " says Meow!";
+}
+
+catTalk();
+
+//scope of let is limited to function or block statement it was declared
+function checkScope() {
+	"use strict";
+	let i = "function scope";
+	if (true) {
+		let i = "block scope"; //using var here would make i block scope in this block AND the function body
+		console.log("scope of i is: ", i);
+	}
+	console.log("scope of i is: ", i);
+	return i;
+}
+
+checkScope();
