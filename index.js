a4f4c0be173a96277188ad0b6627b0d4ca7c4893
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-05 19:59:30 -0600

    Demonstrate Math.random(), Math.floor().

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-//DO-WHILE - always runs at least once BEFORE checking conditional
-var myArray = [];
-var i = 10;
+//MATHS
 
-do {
-	myArray.push(i);
-	i++;
-} while (i < 5);
+function randomFraction() {
 
-console.log(i, myArray); //since i is never less than 5, i is only incremented once.
 
-// ===========================================================================
+	return Math.random(); //returns a number between 0 and 1, non-inclusive
 
-// PROFILE LOOKUP CHALLENGE
-var contacts = [
-	{
-		"firstName": "Akira",
-		"lastName": "Laine",
-		"number": "1234567890",
-		"likes": ["Pizza", "Coding", "Brownie Poitns"]
-	},
-	{
-		"firstName": "Harry",
-		"lastName": "Potter",
-		"number": "1525938182",
-		"likes": ["Hogwarts", "Magic", "Hagrid"]
-	}
-];
-
-function lookupProfile(name, prop) {
-	//user passes in a name and prop, we return the value
-	for (var i = 0; i < contacts.length; i++) {
-		//check if the name exists
-		if (contacts[i].firstName === name) {
-			return contacts[i][prop] || "No such property"
-		} else {
-			return "No such contact"
-		}
-	}
 }
 
-var data = lookupProfile("Akira", "dislikes");
-console.log(data);
+var randomWholeNumber = Math.floor(Math.random() * 20); //between 0 and 19
+
+function generateRandomNumberinRange(ourMin, ourMax) {
+ 	return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+console.log(randomFraction());
+console.log(randomWholeNumber);
+console.log(generateRandomNumberinRange(1, 9));
