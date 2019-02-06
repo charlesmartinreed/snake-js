565bcf6a8f24677418b7e70f85130a58a31f1eee
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 17:41:45 -0600

    Demonstrate class/constructor syntax.

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-//TEMPLATE LITERALS MAKE BUILDING COMPLEX STRINGS EASIER
-const person = {
-	name: "Zodiac Hasbro",
-	age: 56
-};
-
-//make template literals with ``
-//also makes it easy to make multi-line strings
-//now we can use variables within the string, directly
-
-const greeting = `Hello, my name is ${person.name}!
-I am ${person.age} years old.`;
-console.log(greeting);
-
-const result = {
-	success: ["max-length", "no-amd", "prefer-arrow-functions"],
-	failure: ["no-var", "var-on-top", "linebreak"],
-	skipped: ["id-blacklist", "no-dup-keys"]
-};
-
-//makeList function should create a list based on passed in array
-function makeList(arr) {
-	const resultDisplayArray = [];
-
-	for (let i=0; i < arr.length; i++) {
-		resultDisplayArray.push(`<li class="text-warning">${arr[i]}<li>`);
+//objects can contain functions
+const bicycle = {
+	gear: 2,
+	setGear(newGear) {
+		"use strict";
+		this.gear = newGear;
 	}
-
-	return resultDisplayArray;
 }
 
-/*
-	makeList(result.failure) should return:
-	[ `<li class="text-warning">no-var</li>`,
-	  `<li class="text-warning">var-on-top</li>`,
-		`<li class="text-warning">linebreak</li>` ]
-*/
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
-const resultDisplayArray = makeList(result.failure);
-console.log(resultDisplayArray);
+//ES6 can create objects using class keyword
 
-//ES6 added support for easily creating literals
-//JS knows it will return these objects with these var names
-const createPerson = (name, age, gender) => ( { name, age, gender })
-//old method
-	// return {
-	// 	name: name,
-	// 	age: age,
-	// 	gender: gender
-	// };
+//old way of creating objects, using new
+var spaceShuttle = function(targetPlanet) {
+	this.targetPlanet = targetPlanet;
+}
+var zeus = new spaceShuttle('Jupiter'); //create new Object
 
-console.log(createPerson("Zodiac Hasbro", 56, "male"));
+console.log(zeus.targetPlanet)
+
+//new way of creating objects, using class and constructor
+class ES6SpaceShuttle {
+	constructor(targetPlanet) {
+		this.targetPlanet = targetPlanet;
+	}
+}
+var artemis = new ES6SpaceShuttle('Mercury');
+console.log(artemis.targetPlanet);
+
+function makeClass() {
+	class Vegetable {
+		constructor(name) {
+			this.name = name;
+		}
+	}
+
+	return Vegetable;
+}
+
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); //carrot
