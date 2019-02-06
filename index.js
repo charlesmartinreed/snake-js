4c70f8560bff20515cf0ccb16e8781573036db91
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 16:38:40 -0600

    Demonstrate higher order functions.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-/*
-var magic = function() {
-	return new Date();
-};
-*/
+//HIGHER ORDER FUNCTIONS, map, order and reduce
+//these take functions as arguments
 
-//CAN BE CONVERTED INTO ARROW FUNCTIONS
-var magic = () => {
-	return new Date();
-};
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
-//THIS CAN BE SHORTENED EVEN FURTHER
-const MAGIC_CONCISE = () => new Date();
+const squareList = (arr) => {
+	//compute squares of only positive integers - FILTER using arrow func
+	const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+	//result is array with only integers that are greater than 0 and squared
+	return squaredIntegers;
+}
 
-//ARROW FUNCTIONS, LIKE REGULAR FUNCTIONS, CAN ACCEPT ARGUMENTS
-var myConcat = function(arr1, arr2) {
-	return arr1.concat(arr2);
-};
+const squaredIntegers = squareList(realNumberArray); //16, 1764, 36
+console.log(squaredIntegers);
 
-const CONCAT_CONCISE = (arr1, arr2) => arr1.concat(arr2);
+const names = ["Charleston", "David", "Brandice", "Michael"];
 
-console.log(myConcat([1, 2], [3, 4, 5]));
-console.log(CONCAT_CONCISE([1, 2], [3, 4, 5]));
+const filterNames = (name) => {
+	const filtered = name.filter( value => value.length > 5);
+	return filtered;
+}
+console.log(filterNames(names)); //Charleston, Brandice, Michael
