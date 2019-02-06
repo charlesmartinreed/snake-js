a63d5cd9b6837118dfc1c31b97d623d4cf35cf6d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-05 20:17:45 -0600

    Demonstrate nested ternary operators.

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-//parse int takes a string and returns an integer. If conversion not possible, NaN is returned
+//NESTED TERNARIES
 
-function convertToInt(str) {
-	return parseInt(str);
+function checkSign(num) {
+	return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
 }
 
-//radix can be used to change the base value - base-10, base-2, base-8, etc.
-function convertFromBinary(str) {
-	return parseInt(str, 2); //the second param indicates the base to convert FROM
-}
-
-
-
-let value = convertToInt("9");
-let value2 = convertFromBinary("10011"); //19
-
-console.log(value);
-console.log(value2);
-console.log(value3);
+console.log(checkSign(-10));
+console.log(checkSign(20));
+console.log(checkSign(0));
