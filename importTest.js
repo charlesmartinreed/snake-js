05c3f8983482f81c8aa0fd2440814f19df654574
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-06 18:27:10 -0600

    Demonstrate import/export.

---
+++ importTest.js
@@ -0,0 +1,2 @@
+//export means this function can be used by other files
+export const capitalizeString = str => str.toUpperCase();
--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-//GETTERS AND SETTERS
-class Book {
-	constructor(author) {
-		this._author = author; //private variable
-	}
+//IMPORT VS REQUIRE
+//REQUIRE used to be used to used to import functions and code from other files
 
-	//GETTER - return the private variable to the user without giving access to alter it
-	get writer() {
-		return this._author;
-	}
+//export marks something as importable
+//import is used to bring things into a files
 
-	//SETTER - allows setting of private variable without exposing its current contents. Can invoke calculations or just outright overwrite it
-	set writer(updatedAuthor) {
-		this._author = updatedAuthor;
-	}
-}
+// ./ means current directory, .js is implied so don't have to write it
+import { capitalizeString } from "./importTest"
 
-//create thermostat class, make a getter and setter. Accept F, return temp in celsius
-function makeClass() {
-	class Thermostat {
-		constructor(temperature) { //F
-			//convert to C
-			this._temperature = 5/9 * (temperature - 32); //this means only accessible in class, _ means private variable
-		}
-
-		get temperature() {
-			//let celsius = (this._temperature - 32) * (5/9);
-			return this._temperature;
-		}
-
-		set temperature(updatedTemp) {
-			this._temperature = updatedTemp;
-		}
-	}
-
-	return Thermostat;
-}
-
-const Thermostat = makeClass();
-const thermos = new Thermostat(76); //this will be converted to C - 24.44444 C
-let temp = thermos.temperature; //uses getter
-//thermos.temperature = 26; //setter
-//temp = thermos.temperature;
-console.log(temp);
+const cap = capitalizeString("hello!");
+console.log(cap);
